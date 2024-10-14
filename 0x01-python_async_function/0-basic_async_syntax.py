72f4a9964a6a893c0a1b6879e98ed2a64c80887a
Mohamed Mouyahada <mouyahada@gmail.com>
2024-10-14 23:37:07 +0100

    coroutine

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,11 @@
+#!/usr/bin/env python3
 """asynchronous coroutine"""
 import asyncio
 import random
-import time
 
 
-async def wait_random(max_delay=10):
-    start_time = time.time()
+async def wait_random(max_delay: int = 10) -> float:
     """waits for a random delay between 0 and max_delay"""
-    await asyncio.sleep(random.uniform(0, max_delay))
-    return time.time() - start_time
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
